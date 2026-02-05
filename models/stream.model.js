04b6b41c9930fdb53bd5814c6a8b08464a5cc7f2
Markozrm <marcos.ramirez8727@alumnos.udg.mx>
2026-02-05 12:35:49 -0600

    endpoint-saldo-global
    -se implemento el componente para depurar en un filtro el saldo global y otras consultas como las modificaciones manuales

--- models/stream.model.js
+++ models/stream.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const streamSchema = new Schema({
-    id: { 
+    id: {
         type: String, // CAMBIAR DE Number A String
         required: true,
         unique: true
@@ -12,6 +12,22 @@ const streamSchema = new Schema({
     image: { type: String },
     esVIP: { type: Boolean },
     visible: { type: Boolean, default: false }, // Por defecto, un stream no es visible
+    snapshot: {
+        saldoGlobal: Number,
+        retiros: Number,
+        total: Number,
+        startedAt: Date
+    },
+    finalSnapshot: {
+        saldoGlobal: Number,
+        retiros: Number,
+        depositos: Number,
+        saldoManual: Number,
+        restaManual: Number,
+        cazado: Number,
+        total: Number,
+        endedAt: Date
+    },
 }, {
     timestamps: true // Mantiene createdAt y updatedAt
 });
