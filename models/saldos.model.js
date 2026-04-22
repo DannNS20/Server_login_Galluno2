06a73aa9f4b45be19305101910e74b13d7830f45
DannNS20 <danjet2512f@gmail.com>
2026-04-22 13:02:54 -0600

    consultorias

--- models/saldos.model.js
+++ models/saldos.model.js
@@ -6,7 +6,10 @@ const saldosSchema = new Schema({
     usuario: String,
     concepto: { type: String, default: "" },
     tipo: { type: String, default: "" },
-    sala: { type: String}
+    sala: { type: String, default: "" },
+    ronda: { type: Number, default: null },
+    saldo_antes:   { type: Number, default: null },
+    saldo_despues: { type: Number, default: null },
 });
 
-module.exports = model("saldos", saldosSchema); 
+module.exports = model("saldos", saldosSchema);
